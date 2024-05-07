cd072f6f3080a50c8822c3215600456b36d8c015
Nucot-Som <136143072+SomNucot@users.noreply.github.com>
2024-05-07 07:50:18 +0530

    allure reports

--- src/test/java/com/qa/hrm/pagetests/LoginPageTest.java
+++ src/test/java/com/qa/hrm/pagetests/LoginPageTest.java
@@ -5,20 +5,42 @@ import org.testng.annotations.Test;
 
 import com.qa.hrm.applicationconstants.ApplicationConstants;
 
+import io.qameta.allure.Allure;
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Issue;
+import io.qameta.allure.Link;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.TmsLink;
+
 
 
 public class LoginPageTest extends BaseTest {
 	
 	
-//	@Test(groups = {"Regression"})	
-//	public void loginPageCurrentUrl()
-//	{
-//		String actualUrl=loginPage.getLoginPageUrl();
-//		Assert.assertEquals(actualUrl,ApplicationConstants.LOGIN_PAGE_CURRENTURL);
-//		
-//	}
+	@Test(description="Verify login page title",priority=1)
+	@Description("Verify login page title")
+	@Severity(SeverityLevel.NORMAL)
+	@Epic("EPIC001")
+	@Feature("CurrentUrl")
+	public void loginPageCurrentUrl()
+	{
+		
+        
+		String actualUrl=loginPage.getLoginPageUrl();
+		Assert.assertEquals(actualUrl,ApplicationConstants.LOGIN_PAGE_CURRENTURL);
+		
+	}
 	
-	@Test(groups = {"Smoke"})	
+	
+	@Test(description="Verify login page title",priority=2)
+	@Description("Verify login page title")
+	@Severity(SeverityLevel.NORMAL)
+	@Epic("EPIC002")
+	@Feature("PageTitle")
 	public void loginPageTitle()
 	{
 		
@@ -26,7 +48,11 @@ public class LoginPageTest extends BaseTest {
 		Assert.assertEquals(actualTitle,ApplicationConstants.LOGIN_PAGE_TITLE);
 	}
 	
-	@Test(groups = {"Regression"})
+	@Test(description="Verify login page title",priority=3)
+	@Description("Verify login page title")
+	@Severity(SeverityLevel.NORMAL)
+	@Epic("EPIC003")
+	@Feature("LoginPage")
 	public void doLoginPageTest()
 	{
 		loginPage.doLogin(prop.getProperty("username"),prop.getProperty("password"));
@@ -34,6 +60,7 @@ public class LoginPageTest extends BaseTest {
 		
 		
 	}
+	
 	
 	
 	
